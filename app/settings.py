91ae4246db0227909ae39f1c53b51134a77b4b46
Camila Ferreira <camilaferreira68@hotmail.com>
2024-10-22 14:56:58 -0300

    feat: adicionar bootstrap ao projeto

--- app/settings.py
+++ app/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gestao_de_indicadores.apps.GestaoDeIndicadoresConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
