8626f36631fb05e78f21e1c071cea4afae6cec1b
Camila Ferreira <camilaferreira68@hotmail.com>
2024-10-22 01:04:31 -0300

    chore: mapear atributos do modelo

--- gestao_de_indicadores/models.py
+++ gestao_de_indicadores/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.forms.models import model_to_dict
 
 # Create your models here.
 class Setor(models.Model):
     nome = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.nome
+        return str(model_to_dict(self))
