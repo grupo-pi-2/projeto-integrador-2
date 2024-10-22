5413da28e1612daf2e77ac889c6a9ded526dffc5
Camila Ferreira <camilaferreira68@hotmail.com>
2024-10-22 00:18:15 -0300

    feat: criar tabela de setores

--- app/settings.py
+++ app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gestao_de_indicadores.apps.GestaoDeIndicadoresConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
