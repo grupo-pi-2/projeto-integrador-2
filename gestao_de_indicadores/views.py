06e36df471758bbaa18f234b199fe9c809eff3aa
Camila Ferreira <camilaferreira68@hotmail.com>
2024-10-25 02:17:52 -0300

    feat: criar painel de métricas e tabela de serviços (apenas front-end)

--- gestao_de_indicadores/views.py
+++ gestao_de_indicadores/views.py
@@ -6,6 +6,7 @@ from .models import Indicador
 # Create your views here.
 def index(request):
   indicadores = Indicador.objects.all().order_by("created_at")
+  indicador_auditoria = Indicador.objects.get(nome="Auditorias")
   template = loader.get_template("gestao_de_indicadores/index.html")
-  context = { "indicadores": indicadores }
+  context = { "indicadores": indicadores, "indicador_auditoria": indicador_auditoria }
   return HttpResponse(template.render(context, request))
