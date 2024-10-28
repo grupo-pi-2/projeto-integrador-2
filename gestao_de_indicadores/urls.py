f45d856306a49d854dccaa46ab137e64d7b0d418
Camila Ferreira <camilaferreira68@hotmail.com>
2024-10-28 16:46:45 -0300

    feat: criar modal para cadastrar serviço

--- gestao_de_indicadores/urls.py
+++ gestao_de_indicadores/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('busca_indicador/<int:indicador_id>/', views.busca_indicador, name='busca_indicador'),
+    path('lista_clientes/', views.lista_clientes, name='lista_clientes'),
+    path('lista_status_servico/', views.lista_status_servico, name='lista_status_servico'),
+    path('salva_servico/', views.salva_servico, name='salva_servico'),
 ]
--- gestao_de_indicadores/views.py
+++ gestao_de_indicadores/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .forms import ServicoForm
+from django.shortcuts import render, redirect
 from django.template import loader
 
-from .models import Indicador
+from .models import Indicador, Cliente, Servico
 
 # Create your views here.
 def index(request):
@@ -18,3 +19,26 @@ def busca_indicador(request, indicador_id):
   servicos = indicador.servicos.all()
   html = render(request, 'gestao_de_indicadores/indicador.html', {'indicador': indicador, 'servicos': servicos})
   return HttpResponse(html.content, content_type='text/html')
+
+def lista_clientes(request):
+  clientes = Cliente.objects.all()
+  clientes_mapeados = [{"id": cliente.id, "razao_social": f"{cliente.cnpj} - {cliente.razao_social}"} for cliente in clientes]
+  return JsonResponse(clientes_mapeados, safe=False, content_type='application/json')
+
+def lista_status_servico(request):
+  statuses = Servico.Status.choices
+  status_mapeados = [{"id": status[0], "descricao": status[1]} for status in statuses]
+  return JsonResponse(status_mapeados, safe=False, content_type='application/json')
+
+def salva_servico(request):
+  if request.method == 'POST':
+    form = ServicoForm(request.POST)
+
+    if form.is_valid():
+      form.save()
+      return JsonResponse({'success': True, 'indicador': request.POST['indicador']})
+    else:
+      errors = {form.fields[field_name].label: messages for field_name, messages in form.errors.items()}
+      return JsonResponse({'success': False, 'errors': errors })
+  else :
+    redirect('index')
