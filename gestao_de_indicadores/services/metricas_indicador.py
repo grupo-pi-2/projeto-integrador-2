f3ede8e30fdcd1a1dab71aacc5443235cce2a570
Camila Ferreira <camilaferreira68@hotmail.com>
2024-11-12 02:19:36 -0300

    chore: melhorar métodos de métricas

--- gestao_de_indicadores/services/metricas_indicador.py
+++ gestao_de_indicadores/services/metricas_indicador.py
@@ -37,18 +37,14 @@ class MetricasIndicador:
       return self._tempo_total_em_segundos_servicos_concluidos() / self._qtde_servicos_concluidos()
     
     return 0
-  
-  def _tempo_medio_em_horas_servicos_concluidos(self):
+
+  def _tempo_medio_em_horas_e_minutos_servicos_concluidos(self):
     if self._tempo_medio_em_segundos_servicos_concluidos() > 0:
-      return self._tempo_medio_em_segundos_servicos_concluidos() / 3600
+      horas = int(self._tempo_medio_em_segundos_servicos_concluidos() / 3600)
+      minutos = int(self._tempo_medio_em_segundos_servicos_concluidos() % 3600 // 60)
+      return f"{horas:02}:{minutos:02}"
     
-    return 0
-  
-  def _tempo_medio_em_minutos_servicos_concluidos(self):
-    if self._tempo_medio_em_segundos_servicos_concluidos() > 0:
-      return int(self._tempo_medio_em_segundos_servicos_concluidos() % 3600 // 60)
-    
-    return 0
+    return '00:00'
   
   def _tempo_total_em_dias_servicos_concluidos(self):
     return sum(servico.dias_total() for servico in self._servicos_concluidos())
@@ -61,15 +57,16 @@ class MetricasIndicador:
 
   def _tempo_medio_servicos_concluidos(self):
     if self.indicador.tipo_de_tempo_limite == 'horas':
-      return f"{int(self._tempo_medio_em_horas_servicos_concluidos()):02}:{self._tempo_medio_em_minutos_servicos_concluidos():02}"
+      return f'{self._tempo_medio_em_horas_e_minutos_servicos_concluidos()}'
 
     if self.indicador.tipo_de_tempo_limite == 'dias':
       return f'{self._tempo_medio_em_dias_servicos_concluidos()}'
 
   def _percentual_tempo_servicos_concluidos(self):
     if self.indicador.tipo_de_tempo_limite == 'horas':
-      if self.indicador.tempo_limite and self._tempo_medio_em_horas_servicos_concluidos() > 0:
-        return round(self._tempo_medio_em_horas_servicos_concluidos() / self.indicador.tempo_limite, 2) * 100
+      if self.indicador.tempo_limite and self._tempo_medio_em_segundos_servicos_concluidos() > 0:
+        tempo_limite_em_segundos = self.indicador.tempo_limite * 3600
+        return round(self._tempo_medio_em_segundos_servicos_concluidos() / tempo_limite_em_segundos, 2) * 100     
       
       return 0
     
